df72f794e93224d36bc5437c37278af2058f8022
Michael Highsmith <mh902235@2600-6c40-5a00-1c5f-0000-416d-d01b-ad4c.dhcp6.chtrptr.net>
2018-09-25 17:47:26 -0500

    Initial Commit

---
+++ LifeUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeUp
+//
+//  Created by Michael Highsmith on 9/25/18.
+//  Copyright © 2018 Satyr Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
